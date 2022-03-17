1bf7cf8d05be66ffda997ed7c5cc775d2e0a7746
RahmathMarzooka <69783520+RahmathMarzooka@users.noreply.github.com>
2022-03-17 10:45:04 +0530

    implemented resolved issue using redux

--- src/index.js
+++ src/index.js
@@ -13,12 +13,7 @@ store.dispatch(actionCreators.addIssue('one issue added'))
 store.dispatch(actionCreators.addIssue('second issue added'))
 store.dispatch(actionCreators.addIssue('third issue added'))
 
-
-
-
-
 store.dispatch(actionCreators.removeIssue(2))
-
 
 store.dispatch(actionCreators.resolveIssue(3))
 
--- src/reducer.js
+++ src/reducer.js
@@ -2,34 +2,34 @@ import * as actionTypes from './actionTypes'
 
 let id = 1;
 
-function reducer(state =[],action){
-    switch(action.type){
+function reducer(state = [], action) {
+    switch (action.type) {
         case actionTypes.ISSUE_ADDED:
-            return [...state,{
-                id:id++,
-                description:action.payload.description,
-                isResolved:false
+            return [...state, {
+                id: id++,
+                description: action.payload.description,
+                isResolved: false
             }]
 
 
-       case actionTypes.ISSUE_REMOVED:
-           return state.filter(issue =>{
-               return issue.id !== action.payload.id
-           })
-           
-           case actionTypes.ISSUE_RESOLVED:
-           return state.map(issue =>{
-              if(issue.id == action.payload.id){
-                issue.isResolved = true
-              }
-              else{
-                  return state
-              }
+        case actionTypes.ISSUE_REMOVED:
+            return state.filter(issue => {
+                return issue.id !== action.payload.id
+            })
 
-             
-           })
+        case actionTypes.ISSUE_RESOLVED:
+            return state.map(issue => {
+                if (issue.id == action.payload.id) {
+                   return {
+                       ...issue,
+                       isResolved:true
+                   }
+                }else{
+                    return issue;
+                }
+            })
 
-        default:state
+        default: state
     }
     return state
 }
