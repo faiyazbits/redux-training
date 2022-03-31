1be834e0d7a1579aedaa8dc2f63766b5d6956ddc
RahmathMarzooka <69783520+RahmathMarzooka@users.noreply.github.com>
2022-03-31 16:30:41 +0530

    tried recreating monkey patching concept

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import store from "./store";
 
 
-import {setIssues} from "./issues/issue.action"
-import {setProjects,setActiveProject,updatedProject,getProjects} from "./project/project.action"
+import { setIssues } from "./issues/issue.action"
+import { setProjects, setActiveProject, updatedProject, getProjects } from "./project/project.action"
 import * as userActions from "./users/users.action"
 
 
@@ -16,5 +16,21 @@ import { fetchProjects } from "./api";
 
 
 
-store.dispatch(getProjects())
+const database = {
+  rows: [],
+  create(name, age) {
+    this.rows.push(`${name},${age}`)
+  }
+}
 
+function createAndLogUser() {
+  let createUserRef = database.create
+  database.create = function (name, age) {
+    createUserRef.call(database, 'faiyaz', 23)
+    console.log(`user created ${name} ${age}`)
+  }
+}
+
+createAndLogUser()
+database.create('faiyaz', 26)
+database.create('marzooka', 23)
